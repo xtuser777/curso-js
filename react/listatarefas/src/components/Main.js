a2f301c393864286ef4adf449a61b2532c1e0a76
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2022-08-31 21:47:15 -0300

    195. Estilizando o form

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { FaPlus } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
@@ -14,14 +16,17 @@ export default class Main extends Component {
   }
 
   render() {
-    //const { novaTarefa } = this.state;
+    const { novaTarefa } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
-        <form action="#">
-          <input onChange={this.handleChange} type={Text}/>
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}/>
+          <button type="submit"><FaPlus/></button>
         </form>
       </div>
     )
