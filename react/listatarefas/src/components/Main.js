159da1826dc8d8183a617d72d417b494babb238e
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2022-09-01 21:10:19 -0300

    200. Salvando no localStorage

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -9,6 +9,24 @@ export default class Main extends Component {
     novaTarefa: '',
     tarefas: [],
     index: -1,
+  };
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return;
+
+    this.setState({
+      tarefas,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
   }
 
   handleSubmit = (e) => {
