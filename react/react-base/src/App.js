b052ec8c7889d70da003db71ee4490be1d1d5c09
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2022-09-06 23:32:31 -0300

    209. React Router DOM

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -1,14 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
-import Login from './pages/Login';
+import Routes from './routes/index';
 import GlobalStyle from './styles/GlobalStyles';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
 
